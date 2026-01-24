db4de50ffcb7a3f4b488f10210cbcbb5d1ab0569
sheldanx <sheldanx@gmail.com>
2026-01-24 16:06:00 +0700

    Added ListArticles component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 <template>
   <div>
     <PageHeader />
+    <ListArticles />
   </div>
 </template>
 
 <script>
   import PageHeader from '@/components/PageHeader.vue';
+  import ListArticles from './components/ListArticles.vue';
 
   export default {
     components: {
       PageHeader,
+      ListArticles
     },
   }
 </script>
