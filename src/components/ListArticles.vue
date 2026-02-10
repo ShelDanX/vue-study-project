c9d76c8bce493153a6946e88c99701e0a8990a6a
ShelDanX <sheldanx@gmail.com>
2026-02-10 14:48:30 +0700

    Added paginator component

--- src/components/ListArticles.vue
+++ src/components/ListArticles.vue
@@ -7,26 +7,61 @@
           :key="item.id"
           :id="item.id"
           :title="item.title"
-          :description="item.description"
+          :description="item.body"
           class="list-articles__card"
         />
       </div>
+
+      <ThePaginator
+        :page="page"
+        :totalPages="totalPages"
+        class="list-articles__paginator"
+        @setPage="setPage"
+      />
     </div>
   </main>
 </template>
 
 <script>
 import CardArticle from "@/components/card/CardArticle.vue";
-import newsList from "@/json/news.json";
+import ThePaginator from "./ThePaginator.vue";
 
 export default {
   components: {
     CardArticle,
+    ThePaginator,
   },
   data() {
     return {
-      newsList: newsList,
+      newsList: [],
+      limit: 10,
+      page: 1,
+      totalCount: 1,
+      totalPages: 1,
     };
+  },
+  methods: {
+    getNewsList(page = 1) {
+      this.$axios("https://dummyjson.com/posts", {
+        params: {
+          limit: this.limit,
+          skip: this.limit * (page - 1),
+        },
+      }).then((response) => {
+        if (response?.data?.posts) {
+          this.newsList = response.data.posts;
+          this.page = page;
+          this.totalCount = response.data.total;
+          this.totalPages = Math.ceil(this.totalCount / this.limit);
+        }
+      });
+    },
+    setPage(page) {
+      this.getNewsList(page);
+    },
+  },
+  created() {
+    this.getNewsList();
   },
 };
 </script>
